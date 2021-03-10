0fd1f57b1d8302450b75e2c2d5383c897bf89b54
Matteo Foglieni <matteo.foglieni16@gmail.com>
2021-03-10 12:06:55 +0100

    Bug to be resolved

--- src/Raytracing.jl
+++ src/Raytracing.jl
@@ -1,5 +1,14 @@
 module Raytracing
 
+import Colors
+import ColorTypes
+#import Base.:+, Base.:*, Base.:≈
+
+T = Float64
+function Base.:+(x::ColorTypes.RGB{T}, y::ColorTypes.RGB{T})
+     ColorTypes.RGB(x.r + y.r, x.g + y.g, x.b + y.b)
+end
+
 greet() = println("Hello World!")
 
 end # module
