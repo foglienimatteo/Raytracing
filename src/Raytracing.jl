e9f33da7e69dac6980a0341a77382a4000c0a34b
Riccardo Gervasoni <riccardo.gervasoni@studenti.unimi.it>
2021-03-10 12:40:48 +0100

    Fixed bugs

--- src/Raytracing.jl
+++ src/Raytracing.jl
@@ -1,12 +1,12 @@
 module Raytracing
 
-import Colors
-import ColorTypes
-#import Base.:+, Base.:*, Base.:≈
+using Colors  #generico
+import ColorTypes:RGB  #specificare sempre cosa si importa. In questo caso posso evitare di secificare nella funzione "x::ColorTypes.RGB{T}"
+import Base.:+, Base.:*, Base.:≈
 
-T = Float64
-function Base.:+(x::ColorTypes.RGB{T}, y::ColorTypes.RGB{T})
-     ColorTypes.RGB(x.r + y.r, x.g + y.g, x.b + y.b)
+#T = Float64 errato
+function Base.:+(x::RGB{T}, y::RGB{T}) where{T} #in questo modo tipo qualsiasi, per specificare: where{T<:real}
+     RGB(x.r + y.r, x.g + y.g, x.b + y.b)
 end
 
 greet() = println("Hello World!")
