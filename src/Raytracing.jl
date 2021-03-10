4bef43383dc65fff713357b59ba5c24f6539455f
Matteo Foglieni <matteo.foglieni16@gmail.com>
2021-03-10 17:00:24 +0100

    Test added

--- src/Raytracing.jl
+++ src/Raytracing.jl
@@ -2,13 +2,28 @@ module Raytracing
 
 using Colors  #generico
 import ColorTypes:RGB  #specificare sempre cosa si importa. In questo caso posso evitare di secificare nella funzione "x::ColorTypes.RGB{T}"
-import Base.:+, Base.:*, Base.:≈
+import Base.:+, Base.:*, Base.:-, Base.:≈
 
+#=
 #T = Float64 errato
 function Base.:+(x::RGB{T}, y::RGB{T}) where{T} #in questo modo tipo qualsiasi, per specificare: where{T<:real}
      RGB(x.r + y.r, x.g + y.g, x.b + y.b)
 end
+=#
 
-greet() = println("Hello World!")
+Base.:+(a::RGB{T}, b::RGB{T}) where {T} = RGB(a.r + b.r, a.g + b.g, a.b + b.b)
+
+Base.:-(a::RGB{T}, b::RGB{T}) where {T} = RGB(a.r - b.r, a.g - b.g, a.b - b.b)
+
+Base.:*(scalar, c::RGB{T}) where {T} = RGB(scalar*c.r , scalar*c.g, scalar*c.b)
+Base.:*(c::RGB{T}, scalar) where {T} = scalar * c
+
+Base.:≈(a::RGB{T}, b::RGB{T}) where {T} = are_close(a.r,b.r) && are_close(a.g,b.g) && are_close(a.b, b.b)
+
+are_close(x,y,epsilon=1e-10) = abs(x-y) < epsilon
+
+function greet()
+    println("Hello World!")
+end
 
 end # module
---
+++ test/runtests.jl
@@ -0,0 +1,23 @@
+using Raytracing
+using Test
+
+import ColorTypes:RGB
+
+
+@testset "Colors" begin
+	@test 1+1==2
+	@test Raytracing.are_close(1.00000000001, 1)
+
+	a = RGB{Float64}(0.1, 0.2, 0.3)
+	b = RGB{Float64}(0.5, 0.6, 0.7)
+	err = 1e-11
+
+	@test a ≈ RGB{Float64}(0.1 + err, 0.2, 0.3 - 2*err)
+	@test b ≈ RGB{Float64}(0.5, 0.6 + err, 0.7 + 2*err)
+
+	@test a+b ≈ RGB(0.6, 0.8+err, 1.0)
+	@test b-a ≈ RGB(0.4, 0.4-2err, 0.4)
+	@test 2.0*a ≈ RGB(0.2 + err, 0.4, 0.6)
+	@test b*0.5 ≈ RGB(0.25 + err, 0.3, 0.35)
+
+end
