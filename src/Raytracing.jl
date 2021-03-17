0d8f7451e0fe1a72a9919456b4148c11c9cb8dc3
Matteo Foglieni <matteo.foglieni16@gmail.com>
2021-03-17 10:32:10 +0100

    struct HDRimage

--- src/Raytracing.jl
+++ src/Raytracing.jl
@@ -12,18 +12,17 @@ end
 =#
 
 Base.:+(a::RGB{T}, b::RGB{T}) where {T} = RGB(a.r + b.r, a.g + b.g, a.b + b.b)
-
 Base.:-(a::RGB{T}, b::RGB{T}) where {T} = RGB(a.r - b.r, a.g - b.g, a.b - b.b)
-
 Base.:*(scalar, c::RGB{T}) where {T} = RGB(scalar*c.r , scalar*c.g, scalar*c.b)
 Base.:*(c::RGB{T}, scalar) where {T} = scalar * c
-
 Base.:≈(a::RGB{T}, b::RGB{T}) where {T} = are_close(a.r,b.r) && are_close(a.g,b.g) && are_close(a.b, b.b)
-
 are_close(x,y,epsilon=1e-10) = abs(x-y) < epsilon
 
-function greet()
-    println("Hello World!")
+struct HDRimage
+    width
+    heigth
+    Color_matrix::RGB{T}[] where {T}
+
 end
 
 end # module
