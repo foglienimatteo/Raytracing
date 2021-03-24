e032d4cda9ab56ff4b5e11d0620ef03979243d5d
Matteo Foglieni <matteo.foglieni16@gmail.com>
2021-03-24 11:30:37 +0100

    structure of read_pfm added

--- src/Raytracing.jl
+++ src/Raytracing.jl
@@ -4,7 +4,7 @@ using Colors  #generico
 #using IOStream
 import ColorTypes:RGB  #specificare sempre cosa si importa. In questo caso posso evitare di secificare nella funzione "x::ColorTypes.RGB{T}"
 import Base.:+; import Base.:-; import Base.:≈; import Base.:*
-import Base.write
+import Base.write; import Base.read
 
 #=
 #T = Float64 errato
@@ -57,6 +57,11 @@ function print_rgb(c::RGB{T}) where {T}
 end
 
 
+struct InvalidPfmFileFormat <: Exception
+    var::Symbol
+end #InvalidPfmFileFormat
+
+
 function write(io::IO, img::HDRimage)
     endianness=-1.0
     w = img.width
@@ -83,4 +88,49 @@ function write(io::IO, img::HDRimage)
 
 end # write(::IO, ::HDRimage)
 
+
+function read(io::IO, HDRimage)
+    magic = read_line(io)
+    magic == "PF" || throw(InvalidPfmFileFormat("invalid magic number in PFM file: $(magic) instead of 'PF'.\n"))
+
+    img_size = read_line(io)
+    typeof(parse_img_size(img_size)) == Tuple{UInt,UInt} || throw(InvalidPfmFileFormat("invalid img size in PFM file: $(parse_img_size(img_size)) instead of 'Tuple{UInt,UInt}'.\n"))
+    (width, height) = parse_img_size(img_size)
+
+    endianness_line = read_line(io)
+    parse_endianness(endianness_line) == 1.0 || parse_endianness(endianness_line)== -1.0 || throw(InvalidPfmFileFormat("invalid endianness in PFM file: $(parse_endianness(endianness_line)) instead of +1.0 or -1.0.\n"))
+    endianness = parse_endianness(endianness_line)
+
+
+    result = HDRimage(width, height)
+    for y in height-1:-1:0, x in 0:width-1
+        (r,g,b) = [read_float(io, endianness) for i in 0:2]
+        result.set_pixel(x,y,RGB(r,g,b))
+
+    return result
+
+end #read_pfm_image(::IO)
+
+
+#=
+def read_pfm_image(stream):
+    # The first bytes in a binary file are usually called «magic bytes»
+    magic = _read_line(stream)
+    if magic != "PF":
+        raise InvalidPfmFileFormat("invalid magic in PFM file")
+
+    img_size = _read_line(stream)
+    (width, height) = _parse_img_size(img_size)
+
+    endianness_line = _read_line(stream)
+    endianness = _parse_endianness(endianness_line)
+
+    result = HdrImage(width=width, height=height)
+    for y in range(height - 1, -1, -1):
+        for x in range(width):
+            (r, g, b) = [_read_float(stream, endianness) for i in range(3)]
+            result.set_pixel(x, y, Color(r, g, b))
+
+    return result
+=#
 end # module
