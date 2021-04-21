ceadcfe3784a5f2a7069c4499bfbe582e46c00fa
Matteo Foglieni <matteo.foglieni16@gmail.com>
2021-04-21 12:32:15 +0200

    moved

--- src/Raytracing.jl
+++ src/Raytracing.jl
@@ -292,6 +292,22 @@ function overturn(img::HDRimage)
     return IMG
 end
 
+# ----------------------------------------------------------------------------------------------------------------------------------------
+# PRINT VEC, PRINT POINT AND NORM FUNCTIONS
+
+print(io::IO, v::Vec) = (print(io, "Vec:\t ", v.x, "\t", v.y, "\t", v.z); nothing)
+print(v::Vec) = (print(stdout, v); nothing)
+println(v::Vec) = (println(stdout,v); nothing)
+println(io::IO,v::Vec) = (print(io, v); print("\n"); nothing)
+
+print(io::IO, p::Point) = (print(io, "Point:\t ", p.x, "\t", p.y, "\t", p.z); nothing)
+print(p::Point) = (print(stdout, p); nothing)
+println(p::Point) = (println(stdout,p); nothing)
+println(io::IO,p::Point) = (print(io, p); print("\n"); nothing)
+
+squared_norm(v::Union{Vec,Point}) = v.x^2 + v.y^2 + v.z^2
+norm(v::Union{Vec,Point}) = √squared_norm(v)
+normalize(v::Vec) = v/norm(v)
 
 # ----------------------------------------------------------------------------------------------------------------------------------------
 # TRASFORMATION FUNCTIONS
@@ -371,20 +387,6 @@ function is_consistent(T::Trasformation)
     I = SMatrix{4,4}( Diagonal(ones(4)) )
     return p ≈ I
 end
-
-print(io::IO, v::Vec) = (print("Vec:\t ", v.x, "\t", v.y, "\t", v.z); nothing)
-print(v::Vec) = (print(stdout, v); nothing)
-println(v::Vec) = (println(stdout,v); nothing)
-println(io::IO,v::Vec) = (print(io, v); print("\n"); nothing)
-
-print(io::IO, p::Point) = (print("Point:\t ", p.x, "\t", p.y, "\t", p.z); nothing)
-print(p::Point) = (print(stdout, p); nothing)
-println(p::Point) = (println(stdout,p); nothing)
-println(io::IO,p::Point) = (print(io, p); print("\n"); nothing)
-
-squared_norm(v::Union{Vec,Point}) = v.x^2 + v.y^2 + v.z^2
-norm(v::Union{Vec,Point}) = √squared_norm(v)
-normalize(v::Vec) = v/norm(v)
 
 
 
