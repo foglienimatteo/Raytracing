cc6ec174f3ff1db78b20746847807a7114f8ccac
Riccardo Gervasoni <riccardo.gervasoni@studenti.unimi.it>
2021-05-05 12:11:45 +0200

    new structs, operations and a function

--- src/Shapes.jl
+++ src/Shapes.jl
@@ -16,3 +16,8 @@
 # CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
+"""Append a new shape to this world"""
+function add_shape(W::World, S::Shape)
+    push!(W, S)
+    return nothing
+end
