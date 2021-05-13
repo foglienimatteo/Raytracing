52e4a438848890405d8027ccc594875869bfd634
Matteo Foglieni <matteo.foglieni16@gmail.com>
2021-05-13 16:37:22 +0200

    modified ReadingWriting.jl API

--- src/ReadingWriting.jl
+++ src/ReadingWriting.jl
@@ -21,147 +21,175 @@
 """
     valid_coordinates(hdr::HDRimage, x::Int, y::Int) -> Bool
 
-Return `True` if ``(x, y)`` are coordinates within the 2D matrix (in [`HDRimage`](@ref))
+Return `True` if `(x, y)` are valid coordinates for the 
+2D matrix of the [`HDRimage`](@ref)
 """
-valid_coordinates(hdr::HDRimage, x::Int, y::Int) = x>=0 && y>=0 && x<hdr.width && y<hdr.height
+valid_coordinates(hdr::HDRimage, x::Int, y::Int) = 
+    x>=0 && y>=0 && x<hdr.width && y<hdr.height
 
 ##########################################################################################92
 
 """
     pixel_offset(hdr::HDRimage, x::Int, y::Int) -> Int64
 
-Return the position in the 1D array of the specified pixel
+Return the index in the 1D array of the specified pixel `(x, y)` for the 
+given [`HDRimage`](@ref)
 """
-pixel_offset(hdr::HDRimage, x::Int, y::Int) = (@assert valid_coordinates(hdr, x, y); y*hdr.width + (x+1) )
+pixel_offset(hdr::HDRimage, x::Int, y::Int) = 
+    (@assert valid_coordinates(hdr, x, y); y*hdr.width + (x+1) )
 
 ##########################################################################################92
 
 """
     get_pixel(hdr::HDRimage, x::Int, y::Int) -> RBG{Float32}
 
-Return the `Color` value for a pixel in the image
-The pixel at the top-left corner has coordinates (0, 0).
+Return the `RBG{Float32}` color for the `(x, y)` pixel in the given [`HDRimage`](@ref).
+The indexes for a `HDRimage` pixel matrix with `w` width and `h` height 
+follow this sketch:
+```ditaa
+    (0,0) - (0,1) - ... - (0,w)
+    (1,0) - (1,1) - ... - (1,w)
+    ...
+    (h,1) - (h,2) - ... - (h,w)
+```
 """
 get_pixel(hdr::HDRimage, x::Int, y::Int) = hdr.rgb_m[pixel_offset(hdr, x, y)]
 
 ##########################################################################################92
 
 """
-    set_pixel(hdr::HDRimage, x::Int, y::Int, c::RGB{T})
+    set_pixel(hdr::HDRimage, x::Int, y::Int, c::RGB{Float32})
 
-Set the new color for a pixel in the image
-The pixel at the top-left corner has coordinates (0, 0).
+Set the new `RGB{Float32}` color for the `(x, y)` pixel in the given [`HDRimage`](@ref).
+The indexes for a `HDRimage` pixel matrix with `w` width and `h` height 
+follow this sketch:
+```ditaa
+    (0,0) - (0,1) - ... - (0,w)
+    (1,0) - (1,1) - ... - (1,w)
+    ...
+    (h,1) - (h,2) - ... - (h,w)
+```
 """
-set_pixel(hdr::HDRimage, x::Int, y::Int, c::RGB{T}) where {T} = (hdr.rgb_m[pixel_offset(hdr, x,y)] = c; nothing)
+set_pixel(hdr::HDRimage, x::Int, y::Int, c::RGB{Float32}) = 
+    (hdr.rgb_m[pixel_offset(hdr, x,y)] = c; nothing)
 
 ##########################################################################################92
 
 struct InvalidPfmFileFormat <: Exception
     var::String
-end #InvalidPfmFileFormat
+end
 
 ##########################################################################################92
 
 """
     write(io::IO, img::HDRimage)
 
-Write the image in a PFM file
-The `stream` parameter must be a I/O stream. The parameter `endianness` specifies the
-byte endianness to be used in the file.
+Write the given [`HDRimage`](@ref) image using the given `IO` stream.
+The `endianness` used for writing the file is Little Endian (`-1.0`).
 """
 function write(io::IO, img::HDRimage)
     endianness=-1.0
     w = img.width
     h = img.height
+
     # The PFM header, as a Julia string (UTF-8)
     header = "PF\n$w $h\n$endianness\n"
 
     # Convert the header into a sequence of bytes
-    bytebuf = transcode(UInt8, header) # transcode writes a "raw" sequence of bytes (8bit)
+    # PS: transcode writes a "raw" sequence of bytes (8bit)
+    bytebuf = transcode(UInt8, header) 
 
     # Write on io the header in binary code
-    write(io, reinterpret(UInt8, bytebuf))  # reinterpret  writes a "raw" sequence of bytes (8bit)
+    # PS: reinterpret  writes a "raw" sequence of bytes (8bit)
+    write(io, reinterpret(UInt8, bytebuf))  
 
     # Write the image (bottom-to-up, left-to-right)
-    for y in h-1:-1:0, x in 0:w-1                   # !!! Julia conta sempre partendo da 1; prende gli estremi
+    for y in h-1:-1:0, x in 0:w-1         
         color = get_pixel(img, x, y)
-        write(io, reinterpret(UInt8,  [color.r]))   #!!! reinterpret(UInt8, [...]) bisogna specificare il tipo
-        write(io, reinterpret(UInt8,  [color.g]))   # e passargli il vettore [] da cambiare, anche se contiene
-        write(io, reinterpret(UInt8,  [color.b]))   # un solo elemento
+        write(io, reinterpret(UInt8,  [color.r]))  
+        write(io, reinterpret(UInt8,  [color.g]))   
+        write(io, reinterpret(UInt8,  [color.b]))
     end
 
-end # write(::IO, ::HDRimage)
+end
 
 ##########################################################################################92
 
 """
     parse_img_size(line::String) -> (Int64, Int64)
 
-Can interpret the size of an image from a PFM file. Needs a `String`,
-obtained from [`read_line`](@ref)(::IO)
+Return the size (width and height) parsed from a given `String`, throwing 
+`InvalidPfmFileFormat` exception if encounters invalid values.
+It works inside the [`read_line`](@ref)(::IO) function.
 """
 function parse_img_size(line::String)
     elements = split(line, " ")
-    length(elements) == 2 || throw(InvalidPfmFileFormat("invalid image size specification
-                                                        : $(length(elements)) instead of 2"))
+    length(elements) == 2 || throw(InvalidPfmFileFormat(
+                                "invalid image size specification: "*
+                                "$(length(elements)) instead of 2"))
 
     try
         width, height = convert.(Int, parse.(Float64, elements))
         (width > 0 && height > 0) || throw(ErrorException)
         return width, height
     catch e
-        isa(e, InexactError) || throw(InvalidPfmFileFormat("cannot convert width/height
-                                                            $(elements) to Tuple{Int, Int}")
+        isa(e, InexactError) || throw(InvalidPfmFileFormat(
+                                    "cannot convert width/height "*
+                                    "$(elements) to Tuple{Int, Int}")
         )
-        isa(e, ErrorException) || throw(InvalidPfmFileFormat("width/height cannot be negative,
-                                                              but in $(elements) at least one
-                                                              of them is <0.")
+        isa(e, ErrorException) || throw(InvalidPfmFileFormat(
+                                    "width/height cannot be negative,"*
+                                    "but in $(elements) at least one of them is <0.")
         )
     end
 
-end # parse_img_size
+end
 
 ##########################################################################################92
 
 """
     parse_endianness(ess::String) -> Float64
 
-Can understand the endianness of a file; needs a `String`, obtained from [`read_line`](@ref)(`::IO`).
-Returns error if the number is different from ±1.0.
+Return the endianness  parsed from a given `String`, throwing 
+`InvalidPfmFileFormat` exception if encounters an invalid value.
+It works inside the [`read_line`](@ref)(::IO) function.
 """
 function parse_endianness(ess::String)
     try
         val = parse(Float64, ess)
-        (val == 1.0 || val == -1.0) || throw(InvalidPfmFileFormat("invalid endianness in PFM
-                                                                   file: $(parse(Float64, ess))
-                                                                   instead of +1.0 or -1.0.\n")
+        val in [1.0, -1.0] || throw(InvalidPfmFileFormat(
+                                "invalid endianness in PFM file:"*
+                                " $(val) instead of +1.0 or -1.0.\n")
         )
         return val
     catch e
-        throw(InvalidPfmFileFormat("missing endianness in PFM file: $ess instead of ±1.0"))
+        throw(InvalidPfmFileFormat(
+            "missing endianness in PFM file: $ess instead of ±1.0"))
     end
-end # parse_endianness
+end
 
 ##########################################################################################92
 
 """
     read_float(io::IO, ess::Float64) -> Float32
     
-Read a `Float32` from a given IO stream `io` with the (required) endianness `ess`; `ess`
-must be `+1.0` (Big Endian) or `-1.0` (Little Endian).
+Return a `Float32`, readed from the given IO stream `io` with the 
+(required) endianness `ess`; `ess` must be `+1.0` (Big Endian) or 
+`-1.0` (Little Endian).
 
 Controls also if there are enough bits in order to form a `Float32`, 
 otherwise throw `InvalidPfmFileFormat`.
 """
 function read_float(io::IO, ess::Float64)
     # controllo che in ingresso abbia una stringa che sia cnovertibile in Float32
-    ess == 1.0 || ess == -1.0 || throw(InvalidPfmFileFormat("endianness $ess not acceptable."))
+    ess == 1.0 || ess == -1.0 || throw(InvalidPfmFileFormat(
+                                    "endianness $ess not acceptable."))
     try
-        value = read(io, Float32)   # con Float32 leggo già i 4 byte del colore
-        ess == 1.0 ? value = ntoh(value) : value = ltoh(value) # convert machine's endianness
+        value = read(io, Float32) 
+        ess == 1.0 ? value=ntoh(value) : value=ltoh(value) # convert machine's endianness
         return value
     catch e
-        throw(InvalidPfmFileFormat("color is not Float32, it's a $(typeof(io))"))  # ess → io
+        throw(InvalidPfmFileFormat("color is not Float32, it's a $(typeof(io))"))
     end
 end 
 
@@ -193,28 +221,32 @@ end
 
 Read a PFM image from a stream object `io`.
 Return a [`HDRimage`](@ref) object containing the image. If an error occurs, raise a
-``InvalidPfmFileFormat`` exception.
+`InvalidPfmFileFormat` exception.
 
 See also: [`read_line`](@ref)(`::IO`), [`parse_image_size`](@ref)(`::String`),
 [`parse_endianness`](@ref)(`::String`), [`read_float`](@ref)(`::IO, `::Float64`)
 """
 function read(io::IO, ::Type{HDRimage})
+    # magic number
     magic = read_line(io)
-    # lettura numero magico
     magic == "PF" || throw(InvalidPfmFileFormat("invalid magic number in PFM file:"* 
                                                  "$(magic) instead of 'PF'.\n"))
     
-    # lettura dimensioni immagine
+    # image dimension
     img_size = read_line(io)
-    typeof(parse_img_size(img_size)) == Tuple{Int,Int} || throw(InvalidPfmFileFormat("invalid img size in PFM file: $(parse_img_size(img_size)) is $( typeof(parse_img_size(img_size)) ) instead of 'Tuple{UInt,UInt}'.\n"))
+    typeof(parse_img_size(img_size)) == Tuple{Int,Int} || 
+        throw(InvalidPfmFileFormat(
+            "invalid img size in PFM file: $(parse_img_size(img_size)) is "* 
+            "$( typeof(parse_img_size(img_size)) ) instead of 'Tuple{UInt,UInt}'.\n"))
     (width, height) = parse_img_size(img_size)
     
-    #lettura endianness
-    ess_line = read_line(io)
-    parse_endianness(ess_line) == 1.0 || parse_endianness(ess_line)== -1.0 || throw(InvalidPfmFileFormat("invalid endianness in PFM file: $(parse_endianness(ess_line)) instead of +1.0 or -1.0.\n"))
-    endianness = parse_endianness(ess_line)
+    # endianness
+    endianness = parse_endianness(read_line(io))
+    endianness in [1.0, -1.0] || throw(InvalidPfmFileFormat(
+                                "invalid endianness in PFM file: "*
+                                "$(endianness) instead of +1.0 or -1.0.\n"))
 
-    # lettura e assegnazione matrice coloti
+    # color matrix
     result = HDRimage(width, height)
     for y in height-1:-1:0, x in 0:width-1
 
