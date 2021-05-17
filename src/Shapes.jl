0eab81d650a392035afbd47a0821cf9d26b2ac80
Matteo Foglieni <matteo.foglieni16@gmail.com>
2021-05-17 17:57:25 +0200

    added Plane functions

--- src/Shapes.jl
+++ src/Shapes.jl
@@ -36,12 +36,25 @@ end
 
 Convert a 3D point on the surface of the unit sphere into a (u, v) 2D point
 
-See also: [`Point`](@ref), [`Vec2d`](@ref)
+See also: [`Point`](@ref), [`Vec2d`](@ref), [`Sphere`](@ref)
 """
 function sphere_point_to_uv(point::Point)
     u = acos(point.z) / π
     v = atan(point.y, point.x) / (2.0 * π)
     v>=0 ? nothing : v+= 1.0
+    return Vec2d(u,v)
+end
+
+"""
+    plane_point_to_uv(point::Point) -> Vec2d
+
+Convert a 3D point on the surface of the unit plane into a (u, v) 2D point
+
+See also: [`Point`](@ref), [`Vec2d`](@ref), [`Plane`](@ref)
+"""
+function plane_point_to_uv(point::Point)
+    u = point.x - floor(point.x)
+    v = point.y - floor(point.y)
     return Vec2d(u,v)
 end
 
@@ -59,6 +72,21 @@ direction with respect to `ray_dir` ([`Vec`](@ref)).
 function sphere_normal(point::Point, ray_dir::Vec)
     result = Normal(point.x, point.y, point.z)
     Vec(point) ⋅ ray_dir < 0.0 ? nothing : result = -result
+    return result
+end
+
+"""
+    plane_normal(point::Point, ray_dir::Vec) -> Normal
+
+Compute the [`Normal`](@ref) of a unit plane
+
+The normal is computed for [`Point`](@ref) (a point on the surface of the
+plane), and it is chosen so that it is always in the opposite
+direction with respect to `ray_dir` ([`Vec`](@ref)).
+"""
+function plane_normal(point::Point, ray_dir::Vec)
+    result = Normal(0., 0., 1.)
+    Vec(0., 0., 1.) ⋅ ray_dir < 0.0 ? nothing : result = -result
     return result
 end
 
@@ -103,6 +131,35 @@ function ray_intersection(sphere::Sphere, ray::Ray)
         ray
     )
 end
+
+
+"""
+    ray_intersection(plane::Plane, ray::Ray) -> HitRecord
+
+Check if a ray ([`Ray`](@ref)) intersects the plane ([`Plane`](@ref))
+
+Return a [`HitRecord`](@ref), or `nothing` if no intersection is found.
+"""
+function ray_intersection(plane::Plane, ray::Ray)
+    inv_ray = inverse(plane.T) * ray
+
+    !(inv_ray.dir.z ≈ 0.) || (return nothing)
+
+    hit_t = - inv_ray.origin.z / inv_ray.dir.z
+
+    ( (hit_t > inv_ray.tmin) && (hit_t < inv_ray.tmax) ) || (return nothing)
+
+    hit_point = at(inv_ray, hit_t)
+
+    return HitRecord(
+        plane.T * hit_point,
+        plane.T * plane_normal(hit_point, inv_ray.dir),
+        plane_point_to_uv(hit_point),
+        hit_t,
+        ray
+    )
+end
+
 
 ##########################################################################################92
 
