fd53713d883d7f276766b4ea7127ffd1bbce62c1
Matteo Foglieni <matteo.foglieni16@gmail.com>
2021-06-09 10:44:27 +0200

    added GrammarError

--- src/SceneFiles.jl
+++ src/SceneFiles.jl
@@ -4,3 +4,26 @@
 #
 # Copyright © 2021 Matteo Foglieni and Riccardo Gervasoni
 #
+
+"""
+     GrammarError <: Exception(
+          location::SourceLocation
+          message::str
+     )
+
+An error found by the lexer/parser while reading a scene file
+
+### Arguments
+
+- `location::SourceLocation` : a struct containing the name of the file 
+  (or the empty string if there is no real file) and the line and column 
+  number where the error was discovered (both starting from 1)
+
+- `message::String` : a user-frendly error message
+
+See also: [`SourceLocation`](@ref)
+"""
+struct GrammarError <: Exception 
+    location::SourceLocation
+    message::String
+end
